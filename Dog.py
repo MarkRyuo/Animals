5481a9b7252b259b8d8a23984195491ab94e8281
MarkRyuo <malupajhonmark3035@gmail.com>
2024-07-24 14:26:05 +0800

    update herE

--- Dog.py
+++ Dog.py
@@ -3,4 +3,11 @@ from Pets import Pets
 
 class Dog(Pets) :
     
+    # todo create constructor for dog then add the superclass constructor 
+    
+    def __init__(self, name, Type, breed):
+        super().__init__(name, Type)
+        this.breed = breed 
+    
+    
     
