e86db28f0e48d758f4ee8c018f0044948813edcc
MarkRyuo <malupajhonmark3035@gmail.com>
2024-07-25 20:52:20 +0800

    update herE

--- Dog.py
+++ Dog.py
@@ -12,4 +12,4 @@ class Dog(Pets) :
     # Todo call the call method in the superclass 
     
     def call(self):
-        print(f"A {self.Type}, name {self.name}")
+        print(f"A {self.Type}, name {self.name} breed is {self.breed}")
