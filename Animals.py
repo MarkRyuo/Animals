41ff9110d8ed3a0d777499f033d4f8f276f44568
MarkRyuo <malupajhonmark3035@gmail.com>
2024-07-26 18:26:40 +0800

    update here

--- Animals.py
+++ Animals.py
@@ -9,8 +9,11 @@ class Animals(ABC) :
     def __init__(self, Type, name, age) :
         self.Type = Type  # ? A public attribute 
         self.name = name  # ? A public attribute 
-        self.__age = age  # ? A private attribute 
+        self.__age = age  # ? A private attribute (age is not accessible in subclass)
     
     @abstractmethod
     def call(self) :
         pass
+
+    def get_age(self, age) :
+        return 
