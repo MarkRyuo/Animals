66a5b68397c2498456e158e09d9ef5b8e04d0c2f
MarkRyuo <malupajhonmark3035@gmail.com>
2024-07-26 18:30:04 +0800

    update here

--- Dog.py
+++ Dog.py
@@ -2,3 +2,12 @@ from Animals import Animals
 
 
 # Todo creating a subclass for Animals 
+
+class Dog(Animals):
+
+    def __init__(self, Type, name, age):
+        super().__init__(Type, name, age)
+    
+
+    def call(self) :
+        
