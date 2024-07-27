a90da5d48fb4494630c82ce649c11dcb492366b6
MarkRyuo <malupajhonmark3035@gmail.com>
2024-07-27 09:58:40 +0800

    update here

--- Main.py
+++ Main.py
@@ -4,8 +4,6 @@ from Dog import Dog
 def main() :
 
     dog = Dog("Rambo", "Dog", 30)
-    dog.call()
-    print(dog.get_age())
 
     if dog :
         print(f"{dog.call()} and the age is {dog.get_age()}")
