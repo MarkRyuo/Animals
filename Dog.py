bb60e01873790beaae8c567a683c9927d5edeb41
MarkRyuo <malupajhonmark3035@gmail.com>
2024-07-27 09:41:00 +0800

    2nd day for Encapsulation

--- Dog.py
+++ Dog.py
@@ -8,9 +8,6 @@ class Dog(Animals):
     def __init__(self, Type, name, age):
         super().__init__(Type, name, age)
     
-
-    def call(self) :
-        print(f"The name of this {self.Type} is {self.name}") # ! Solve this problem
     
     def get_age(self):
         return self.__age
