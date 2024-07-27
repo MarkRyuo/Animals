043b22ed3711e480f78d7ed4fc9c56b4c2e22026
MarkRyuo <malupajhonmark3035@gmail.com>
2024-07-27 09:50:27 +0800

    update here

--- Dog.py
+++ Dog.py
@@ -10,4 +10,4 @@ class Dog(Animals):
     
     
     def call(self) :
-        print(f"The name of this {self.Type} is {self.name}") # ! Solve this problem
+        print(f"The name of this ({self.Type}) is ({self.name})") # ! Solve this problem
