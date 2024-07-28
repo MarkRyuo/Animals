a7ece3125e8f341af0201523c1dc184da0f7347d
MarkRyuo <malupajhonmark3035@gmail.com>
2024-07-28 18:16:46 +0800

    Version 4

--- Main.py
+++ Main.py
@@ -7,7 +7,7 @@ def main() :
 
     if dog : # * Creating a statement 
         print(f"{dog.call()} and the age is ({dog.get_age()})")
-        print()
+        print(dog.get_owner)
     else :
         return False
 
