7f1ff341fd60b1cb197e05370847a8d083f0937f
MarkRyuo <malupajhonmark3035@gmail.com>
2024-07-28 18:11:04 +0800

    Version 4

--- Main.py
+++ Main.py
@@ -3,7 +3,7 @@ from Dog import Dog
 
 def main() :
 
-    dog = Dog("Dog", "Rambo", 30)
+    dog = Dog("Dog", "Rambo", 30,"Moda")
 
     if dog : # * Creating a statement 
         print(f"{dog.call()} and the age is ({dog.get_age()})")
