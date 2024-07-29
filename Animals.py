8e90eb1678217804f611ce055b40cd3c56624677
MarkRyuo <malupajhonmark3035@gmail.com>
2024-07-29 21:20:56 +0800

    update herE

--- Animals.py
+++ Animals.py
@@ -6,6 +6,6 @@ class Animals(ABC) :
     # Todo Name_of_animal, age_of_animal, breed_of_animal
 
     def __init__(self, animalName,animalAge,animalBreed) :
-        self.animalName = animalName 
+        self.animalName = animalName # ? Public attribute
         self.animalAge = animalAge # ? Public attribute
         self.__animalBreed = animalBreed # ? Private attribute 
