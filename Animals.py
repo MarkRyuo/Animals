364c46bbf25a237a6806653f84e820ba10eab459
MarkRyuo <malupajhonmark3035@gmail.com>
2024-07-29 21:19:54 +0800

    update herE

--- Animals.py
+++ Animals.py
@@ -5,6 +5,7 @@ class Animals(ABC) :
 
     # Todo Name_of_animal, age_of_animal, breed_of_animal
 
-    def __init__(self, animalName,animalAge,animalbreed) :
+    def __init__(self, animalName,animalAge,animalBreed) :
         self.animalName = animalName 
-        
+        self.animalAge = animalAge
+        self.animalBreed = 
