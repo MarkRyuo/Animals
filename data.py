63cc3a382170d275f307dce7928ade698d865dbd
MarkRyuo <malupajhonmark3035@gmail.com>
2024-07-30 18:31:37 +0800

    update herE

--- data.py
+++ data.py
@@ -4,10 +4,14 @@
 
 # todo create a list or dict of animals data
 
+# Todo create nested dict 
 
 data_Animals = {
+
+    "animal_1" : {
     "nameData" : "Rambo",
     "ageData" : 10,
     "breedData" : "Askal"
+    }
 }
 
