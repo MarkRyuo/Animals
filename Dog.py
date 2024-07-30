9e5a817c2170680f8f1e571bfae2ba7b81945ffc
MarkRyuo <malupajhonmark3035@gmail.com>
2024-07-30 17:53:41 +0800

    update herE

--- Dog.py
+++ Dog.py
@@ -6,4 +6,9 @@ class Dog(Animals) :
 
     # Todo create constructor for Dog ang import superclass 
 
+    def __init__(self, animalName, animalAge, animalBreed) :
+        super().__init__(animalName, animalAge, animalBreed) 
     
+
+    def display(self):
+        return super().display()
